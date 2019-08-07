28d17fa3b8af72499b5a44dbbc15e1db0d75b275
Pacificrus <pacificrus63@yandex.ru>
2019-08-08 01:46:27 +0400

    Disable Spring Boot banner on startup

--- applications/client/src/main/java/com/miro/leshukovma/chat/client/ChatClientMain.java
+++ applications/client/src/main/java/com/miro/leshukovma/chat/client/ChatClientMain.java
@@ -1,11 +1,14 @@
 package com.miro.leshukovma.chat.client;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ChatClientMain {
     public static void main(String[] args) {
-        SpringApplication.run(ChatClientMain.class, args);
+        SpringApplication springApplication = new SpringApplication(ChatClientMain.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+        springApplication.run(args);
     }
 }
